e5369432152176ffe69d1e4579900833f5e99d37
Joshua Colvin <joshicolvin@gmail.com>
2025-02-24 03:53:58 -0500

    Added view templates and appropriate styling. Passed fake data to templates for testing.

--- router.js
+++ router.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const Router = express.Router();
+// Include pug library for generating and passing data to templates
+const pug = require('pug');
+
+const LOGIN_PAGE = pug.compileFile('./views/login.pug');
+const ROOMS_PAGE = pug.compileFile('./views/rooms.pug');
+const ROOM_PAGE = pug.compileFile('./views/room.pug');
 
 Router.get('/',(req,res) => {
-  res.end('<h1>Home login page</h1>');
+  res.end(LOGIN_PAGE());
 });
 Router.post('/login', (req,res) => {
   console.log('Attempting to login');
   res.redirect('/rooms');
 });
 Router.get('/rooms', (req,res) => {
-  res.end('<h1>Room List</h1>');
+  const rooms = [
+    {
+      id: 1,
+      name: 'Room1',
+      num_users: 1,
+      max_num_users: 2
+    },
+    {
+      id: 2,
+      name: 'Room2',
+      num_users: 4,
+      max_num_users: 8
+    }
+  ];
+
+  res.end(
+    ROOMS_PAGE({
+      rooms
+    })
+  );
 })
 Router.post('/rooms', (req,res) => {
   console.log('Creating new room');
@@ -20,7 +45,12 @@ Router.post('/rooms', (req,res) => {
 Router.get('/room/:roomID', (req,res) => {
   const { roomID } = req.params;
 
-  res.end(`<h1>Room: ${roomID}`);
+  let data = {
+    username: 'John',
+    roomname: 'Chatsphere'
+  }
+
+  res.end(ROOM_PAGE(data));
 })
 // Redirect any other incorrect path traffic to the home page
 Router.use('*', (req,res) => {
